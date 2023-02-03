e81b77d334c0848c2154fdfb7fbb774092093226
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-02-03 13:50:54 +0100

    Extracted curled script to adjust to my needs

--- ssh-key.sh
+++ ssh-key.sh
@@ -6,7 +6,18 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-curl https://raw.githubusercontent.com/driesvints/dotfiles/HEAD/ssh.sh | sh -s "$1"
+# Generating a new SSH key
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
+ssh-keygen -t ed25519 -C "$1" -f ~/.ssh/id_ed25519
+
+# Adding your SSH key to the ssh-agent
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+eval "$(ssh-agent -s)"
+
+touch ~/.ssh/config
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" | tee ~/.ssh/config
+
+ssh-add -K ~/.ssh/id_ed25519
 
 # copy the key to clipboard
 pbcopy < ~/.ssh/id_ed25519.pub
