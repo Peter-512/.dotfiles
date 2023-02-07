8efa979a72ec45a5d5703a81ccb38e615ea3d87c
Peter Buschenreiter <63590579+Peter-512@users.noreply.github.com>
2023-02-07 13:23:46 +0100

    Some fixes to the way ssh key is set up

--- install.sh
+++ install.sh
@@ -10,22 +10,22 @@ echo "Installing Xcode Command Line Tools for git and homebrew"
 xcode-select --install
 
 echo "Setting up the ssh key..."
-./ssh-key.sh "$1"
+curl https://raw.githubusercontent.com/Peter-512/.dotfiles/main/ssh-key.sh | sh -s "$1"
 
 echo "Cloning dotfiles repo..."
 git clone git@github.com:Peter-512/.dotfiles.git ~/.dotfiles
 
 echo "Linking dotfiles..."
-./link.sh
+~/.dotfiles/link.sh
 
 echo "Setting up macOS defaults..."
-./set-defaults.sh
+~/.dotfiles/set-defaults.sh
 
 echo "Installing homebrew..."
-./brew.sh
+~/.dotfiles/brew.sh
 
 echo "Installing oh-my-zsh..."
-./oh-my-zsh.sh
+~/.dotfiles/oh-my-zsh.sh
 
 echo raycast://extensions/raycast/raycast/import-settings-data | pbcopy
 echo "Raycast command has been copied into your clipboard. Paste it into Raycast and select 'Open in Browser' to run. Then select the .rayconfig file in the repo to import the settings."
