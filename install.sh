9ac54e23f16863bac6c941310a2319b97457e3d9
Peter Buschenreiter <63590579+Peter-512@users.noreply.github.com>
2023-03-22 21:43:21 +0100

    made some visual changes along with and extra wait for input to allow for time to paste the ssh key to github

--- install.sh
+++ install.sh
@@ -9,23 +9,52 @@ fi
 echo "Installing Xcode Command Line Tools for git and homebrew"
 xcode-select --install
 
+echo "
+-------------------
+"
+
 echo "Setting up the ssh key..."
 curl https://raw.githubusercontent.com/Peter-512/.dotfiles/main/ssh-key.sh | sh -s "$1"
+read -p "Waiting for you to paste the key into github..."
+
+echo "
+-------------------
+"
 
 echo "Cloning dotfiles repo..."
 git clone git@github.com:Peter-512/.dotfiles.git ~/.dotfiles
 
+echo "
+-------------------
+"
+
 echo "Linking dotfiles..."
 ~/.dotfiles/link.sh
+
+echo "
+-------------------
+"
 
 echo "Setting up macOS defaults..."
 ~/.dotfiles/set-defaults.sh
 
+echo "
+-------------------
+"
+
 echo "Installing homebrew..."
 ~/.dotfiles/brew.sh
 
+echo "
+-------------------
+"
+
 echo "Installing oh-my-zsh..."
 ~/.dotfiles/oh-my-zsh.sh
+
+echo "
+-------------------
+"
 
 echo raycast://extensions/raycast/raycast/import-settings-data | pbcopy
 echo "Raycast command has been copied into your clipboard. Paste it into Raycast and select 'Open in Browser' to run. Then select the .rayconfig file in the repo to import the settings."
