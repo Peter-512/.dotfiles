741c9d81de3f37d39b5b15d28371392f9e7f3681
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-04-05 12:06:55 +0200

    adjusted private key permissions

--- ssh-key.sh
+++ ssh-key.sh
@@ -7,6 +7,7 @@ ansible-vault decrypt ~/.ssh/id_ed25519.pub
 
 curl https://raw.githubusercontent.com/Peter-512/.dotfiles/main/id_ed25519 > ~/.ssh/id_ed25519
 ansible-vault decrypt ~/.ssh/id_ed25519
+chmod 700 ~/.ssh/id_ed25519
 
 # Adding your SSH key to the ssh-agent
 # https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
