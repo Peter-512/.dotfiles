f2a8404e1bfa9ecbfc3023a2e79bb91063ffba0a
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-04-05 12:09:00 +0200

    installing brew and brewfile with sudo 🤞🏻

--- brew.sh
+++ brew.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 curl https://raw.githubusercontent.com/Peter-512/.dotfiles/main/Brewfile > ~/Brewfile
 
 # Pass the Brewfile location
-brew bundle --file ~/Brewfile
+sudo brew bundle --file ~/Brewfile
