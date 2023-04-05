b4fc957e165b807817001ac005fa10beaeb2dcef
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-04-05 12:16:54 +0200

    testing brew shit

--- brew.sh
+++ brew.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+whoami
+
 # Install Homebrew
-sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 curl https://raw.githubusercontent.com/Peter-512/.dotfiles/main/Brewfile > ~/Brewfile
 
 # Pass the Brewfile location
-sudo brew bundle --file ~/Brewfile
+brew bundle --file ~/Brewfile
