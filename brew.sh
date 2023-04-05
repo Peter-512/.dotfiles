d61ee55b4de3b74121010d328953991098207ccf
Peter Buschenreiter <63590579+Peter-512@users.noreply.github.com>
2023-04-05 10:12:53 +0200

    fix download of homebrew

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Install Homebrew
-/bin/bash - c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Pass the Brewfile location
 brew bundle --file ~/.dotfiles/Brewfile
